f8f4cd8ef97413e3dd634a364e27d70411a801a5
Vincenzo Monopoli <monovinci@gmail.com>
2024-09-29 04:59:58 +0200

    Aggiunto comando (commentato) per usare requirements.py anche su Linux-based system

--- prediction.py
+++ prediction.py
@@ -20,6 +20,8 @@ face = ap.extract_face(img_path, "./test/tmp/")
 if face is True:
 	import subprocess
 	os.chdir('face-parsing')
+	# subprocess.run(['python', 'main.py', '--image', '../test/tmp', '--output', '../test/tmp'])
+	# Se si runna il programma da wls o linux commentare la riga di sotto e 'scommentare quella di sopra'
 	subprocess.run('python main.py --image ../test/tmp --output ../test/tmp')
 	os.chdir('..')
 
